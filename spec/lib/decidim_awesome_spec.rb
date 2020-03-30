48327fc3c3cbfe3ff78e395c5e1c95cb96bb5c89
Ivan Vergés <ivan@platoniq.net>
2020-03-30 18:25:05 +0200

    add _head to md5 test

--- spec/lib/decidim_awesome_spec.rb
+++ spec/lib/decidim_awesome_spec.rb
@@ -13,12 +13,13 @@ module Decidim
 
     files_md5 = {
       "#{admin.gem_dir}/app/views/layouts/decidim/admin/_header.html.erb" => "6f0c010b1dcf912be1b3e0ff9f156026",
+      "#{core.gem_dir}/app/views/layouts/decidim/_head.html.erb" => "b261c55492d047ba730eed8b39be0304",
       "#{core.gem_dir}/app/assets/javascripts/decidim/editor.js.es6" => "797d0ec1c9e79453cf6718f82d2fdd27"
     }
 
     it "each file matches the expected checksum" do
       files_md5.each do |file, md5|
-        expect(md5).to eq md5(file)
+        expect(md5(file)).to eq md5
       end
     end
 
