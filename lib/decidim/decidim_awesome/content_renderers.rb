b2b3665e2d1fa5af272f3dc9da7770f4737148ce
Ivan Vergés <ivan@platoniq.net>
2021-04-13 14:53:01 +0200

    refactor markdown editor behaviour. convert to html before saving

--- lib/decidim/decidim_awesome/content_renderers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Decidim
-  module DecidimAwesome
-    module ContentRenderers
-      autoload :MarkdownRenderer, "decidim/decidim_awesome/content_renderers/markdown_renderer"
-    end
-  end
-end
