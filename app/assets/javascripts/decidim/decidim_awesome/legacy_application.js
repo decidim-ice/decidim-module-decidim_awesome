4f2a43a8a64a60b9901c14bc0c0d515d3218e17b
Ivan Vergés <ivan@platoniq.net>
2021-04-13 20:06:29 +0200

    remove legacy markdown calls

--- app/assets/javascripts/decidim/decidim_awesome/legacy_application.js
+++ app/assets/javascripts/decidim/decidim_awesome/legacy_application.js
@@ -1,4 +1,3 @@
 // = require decidim/decidim_awesome/proposals/images
 // = require decidim/decidim_awesome/editors/legacy_quill_editor
-// = require decidim/decidim_awesome/editors/markdown_view
 // = require decidim/decidim_awesome/forms/autosave
