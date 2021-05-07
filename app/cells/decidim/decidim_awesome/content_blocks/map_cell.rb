02b8e6b32d89df94febf6567d0cb60327f358e9f
Vera Rojman <vrojman@protonmail.com>
2021-05-07 18:26:55 +0200

    Load all categories from organization

--- app/cells/decidim/decidim_awesome/content_blocks/map_cell.rb
+++ app/cells/decidim/decidim_awesome/content_blocks/map_cell.rb
@@ -11,6 +11,18 @@ module Decidim
 
         delegate :snippets, to: :controller
 
+        def all_categories
+          return if @all_categories.present?
+
+          @category_ids ||= Decidim::Category.pluck(:id, :decidim_participatory_space_type, :decidim_participatory_space_id).select do |category|
+            id, space_type, space_id = category
+            space = space_type.constantize.find(space_id)
+            space.organization == current_organization
+          end.map(&:first)
+
+          @all_categories ||= Decidim::Category.where(id: @category_ids)
+        end
+
         def global_map_components
           @global_map_components ||= Decidim::Component.where(manifest_name: "meetings").published
         end
