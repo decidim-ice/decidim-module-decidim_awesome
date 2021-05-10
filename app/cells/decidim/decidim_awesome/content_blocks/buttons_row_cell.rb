aa5b5a7140dd4a0a48295b03b02a87f7d468b1a0
Vera Rojman <vrojman@protonmail.com>
2021-05-10 17:30:02 +0200

    Don't render empty buttons

--- app/cells/decidim/decidim_awesome/content_blocks/buttons_row_cell.rb
+++ app/cells/decidim/decidim_awesome/content_blocks/buttons_row_cell.rb
@@ -6,17 +6,24 @@ module Decidim
       class ButtonsRowCell < Decidim::ViewModel
         BUTTONS_COUNT = 5
 
+        def show
+          render if section_title.present? && buttons.present?
+        end
+
         def section_title
           translated_attribute(model.settings.title)
         end
 
+        def buttons_data
+          1.upto(BUTTONS_COUNT).map do |x|
+            data = %w(text url).map { |field| translated_attribute(model.settings.send(:"button_#{x}_#{field}")) }
+            data unless data.any?(&:blank?)
+          end.compact
+        end
+
         def buttons
           safe_join(
-            1.upto(BUTTONS_COUNT).map do |x|
-              next unless translated_attribute(model.settings.send(:"button_#{x}_text"))
-
-              link_to translated_attribute(model.settings.send(:"button_#{x}_text")), translated_attribute(model.settings.send(:"button_#{x}_url")), class: "button button--sc expanded"
-            end.compact
+            buttons_data.map { |button| link_to button.first, button.last, class: "button button--sc expanded" }
           )
         end
       end
