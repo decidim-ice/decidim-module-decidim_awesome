01b3caccaf1885d6903bdb5de34b0f86dda07105
Ivan Vergés <ivan@platoniq.net>
2021-06-17 15:57:46 +0200

    add cell test

--- spec/cells/content_blocks/map_cell_spec.rb
+++ spec/cells/content_blocks/map_cell_spec.rb
@@ -2,29 +2,40 @@
 
 require "spec_helper"
 
-describe Decidim::DecidimAwesome::ContentBlocks::MapCell, type: :cell do
-  subject { cell(content_block.cell, content_block).call }
+module Decidim::DecidimAwesome
+  describe ContentBlocks::MapCell, type: :cell do
+    subject { cell(content_block.cell, content_block).call }
 
-  let(:organization) { create(:organization) }
-  let(:content_block) { create :content_block, organization: organization, manifest_name: :awesome_map, scope_name: :homepage, settings: settings }
-  let(:settings) { {} }
+    let(:organization) { create(:organization) }
+    let(:content_block) { create :content_block, organization: organization, manifest_name: :awesome_map, scope_name: :homepage, settings: settings }
+    let(:settings) { {} }
 
-  controller Decidim::PagesController
+    controller Decidim::PagesController
 
-  before do
-    allow(controller).to receive(:current_organization).and_return(organization)
-  end
-
-  context "when the content block has customized the max results setting value" do
-    # note that settings is doing nothing here, just left it for when conferences block is improved
-    let(:settings) do
-      {
-        "max_results" => "8"
-      }
+    before do
+      allow(controller).to receive(:current_organization).and_return(organization)
     end
 
-    it "shows up to 8 conferences" do
-      expect(highlighted_conferences).to have_selector("a.card--conference", count: 5)
+    it "shows the map" do
+      expect(subject).to have_selector("#awesome-map")
+      expect(subject).to have_content("window.AwesomeMap.categories")
+    end
+
+    it "do not show the title" do
+      expect(subject).not_to have_selector("h3.section-heading")
+    end
+
+    context "when the content block has a title" do
+      let(:settings) do
+        {
+          "title" => {"en" => "Look this beautiful map!"}
+        }
+      end
+
+      it "shows the title" do
+        expect(subject).to have_selector("h3.section-heading")
+        expect(subject).to have_content("Look this beautiful map!")
+      end
     end
   end
 end
