63cf02922f04fd354ded1e6153c0025568ca9dbc
Ivan Vergés <ivan@platoniq.net>
2021-06-18 15:07:10 +0200

    fix conflic solving error

--- app/assets/config/decidim_decidim_awesome_manifest.js
+++ app/assets/config/decidim_decidim_awesome_manifest.js
@@ -2,4 +2,4 @@
 // = link decidim/decidim_awesome/application.js
 // = link decidim/decidim_awesome/awesome_map/map.js
 // = link decidim/decidim_awesome/awesome_map/load_map.js
-// = link decidim/decidim_awesome/proposals/custom_f
+// = link decidim/decidim_awesome/proposals/custom_fields.js
