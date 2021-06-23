e5875a110b501ea9364a0167f7bc280265c59b69
Ivan Vergés <ivan@platoniq.net>
2021-06-23 15:27:55 +0200

    leave user untampered

--- app/awesome_overrides/models/decidim/user_override.rb
+++ app/awesome_overrides/models/decidim/user_override.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Decidim::User.class_eval do
-  # TODO: Assign admin if is in a constraint
+  # TODO: Assign admin if is a constraint exists for the current_context
+  # TODO: add a middleware to store the current context
   def admin
-    false
+    # no context available, return default attribute
+    read_attribute("admin")
   end
 
   def admin?
-    admin
+    admin.present?
   end
 end
