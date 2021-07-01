d0e8fe9e30fa939dc41cf6904d8c10c3043054cd
Vera Rojman <vrojman@protonmail.com>
2021-07-01 19:24:22 +0200

    Change "limited admins" to "scoped admins" for consistency

--- lib/decidim/decidim_awesome/test/shared_examples/scoped_admins_examples.rb
+++ lib/decidim/decidim_awesome/test/shared_examples/scoped_admins_examples.rb
@@ -126,7 +126,7 @@ shared_examples "allows all admin routes" do
   end
 end
 
-shared_examples "allows limited admin routes" do
+shared_examples "allows scoped admin routes" do
   before do
     visit decidim_admin.root_path
   end
