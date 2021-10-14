0032609bbd5c3974354df0ba12a9a426bedb2f5c
Ivan Vergés <ivan@platoniq.net>
2021-10-14 19:34:03 +0200

    refactor proposal wizard form

--- app/awesome_overrides/forms/decidim/proposals/proposal_wizard_create_step_form_override.rb
+++ app/awesome_overrides/forms/decidim/proposals/proposal_wizard_create_step_form_override.rb
@@ -1,46 +1,22 @@
 # frozen_string_literal: true
 
-# Body valitation are overriden with a custom html/xml structure to store
-# the custom fields definition
-class Decidim::Proposals::ProposalWizardCreateStepForm < Decidim::Form
-  mimic :proposal
+# Recreate validations to take into account custom fields and ignore the length limit in proposals
+Decidim::Proposals::ProposalWizardCreateStepForm.class_eval do
+  clear_validators!
 
-  attribute :title, String
-  attribute :body, defined?(Decidim::Attributes::CleanString) ? Decidim::Attributes::CleanString : String
-  attribute :body_template, String
-  attribute :user_group_id, Integer
-
-  validates :title, presence: true, etiquette: true
-  validates :body, presence: true, etiquette: true, unless: ->(form) { form.override_validations? }
+  validates :title, :body, presence: true, etiquette: true
   validates :title, length: { in: 15..150 }
   validates :body, proposal_length: {
     minimum: 15,
-    maximum: ->(record) { record.component.settings.proposal_length }
-  }, unless: ->(form) { form.override_validations? }
+    maximum: ->(record) { record.override_validations? ? 0 : record.component.settings.proposal_length }
+  }
 
   validate :body_is_not_bare_template, unless: ->(form) { form.override_validations? }
-
-  alias component current_component
-
-  def map_model(model)
-    self.user_group_id = model.user_groups.first&.id
-    return unless model.categorization
-
-    self.category_id = model.categorization.decidim_category_id
-  end
 
   def override_validations?
     return false if context.current_component.settings.participatory_texts_enabled
 
     custom_fields.present?
-  end
-
-  private
-
-  def body_is_not_bare_template
-    return if body_template.blank?
-
-    errors.add(:body, :cant_be_equal_to_template) if body.presence == body_template.presence
   end
 
   def custom_fields
