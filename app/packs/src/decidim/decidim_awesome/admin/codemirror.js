4124d01cb8359d481d2229e39ec0b49e24798310
Ivan Vergés <ivan@platoniq.net>
2022-01-28 22:26:57 +0100

    fix codemirror

--- app/packs/src/decidim/decidim_awesome/admin/codemirror.js
+++ app/packs/src/decidim/decidim_awesome/admin/codemirror.js
@@ -1,9 +1,7 @@
-// TODO
-// = require mode/css/css
-// = require keymap/sublime
-// TODO
-
-import "codemirror"
+import CodeMirror from "codemirror"
+import "codemirror/mode/css/css"
+import "codemirror/keymap/sublime"
+import "stylesheets/decidim/decidim_awesome/admin/codemirror.scss";
 
 $(() => {
   $(".awesome-edit-config .scoped_styles_container textarea").each((_idx, el) => {
