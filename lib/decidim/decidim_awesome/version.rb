5c0e70ebc7bb833fb50c6b05de4e13b495054c86
Alexander Rusa <alex@rusa.at>
2022-12-12 08:21:08 +0000

    Bump decidim version

--- lib/decidim/decidim_awesome/version.rb
+++ lib/decidim/decidim_awesome/version.rb
@@ -4,6 +4,6 @@ module Decidim
   # This holds the decidim-decidim_awesome version.
   module DecidimAwesome
     VERSION = "0.8.3"
-    COMPAT_DECIDIM_VERSION = [">= 0.25.0", "< 0.27"].freeze
+    COMPAT_DECIDIM_VERSION = [">= 0.25.0", "< 0.28"].freeze
   end
 end
