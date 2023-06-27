2036a2342c25e912d967addab6aabc228b361808
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-06-27 17:00:08 +0200

    tests added for new functionality

--- spec/system/awesome_map_spec.rb
+++ spec/system/awesome_map_spec.rb
@@ -10,24 +10,48 @@ describe "Show awesome map", type: :system do
   let!(:proposal) { create(:proposal, component: proposal_component, latitude: 40, longitude: 2) }
   let(:emendation) { build(:proposal, component: proposal_component, latitude: 42, longitude: 4) }
   let!(:proposal_amendment) { create(:proposal_amendment, amendable: proposal, emendation: emendation) }
-
+  let!(:accepted_proposal) { create(:proposal, :accepted, component: proposal_component, latitude: 50, longitude: 2) }
+  let!(:evaluating_proposal) { create(:proposal, :evaluating, component: proposal_component, latitude: 48, longitude: 4) }
+  let!(:not_answered_proposal) { create(:proposal, :not_answered, component: proposal_component, latitude: 60, longitude: 6) }
+  let!(:withdrawn_proposal) { create(:proposal, :withdrawn, component: proposal_component, latitude: 60, longitude: 2) }
+  let!(:rejected_proposal) { create(:proposal, :rejected, component: proposal_component, latitude: 10, longitude: 2) }
   let!(:category) { create(:category, participatory_space: participatory_process) }
   let!(:subcategory) { create(:subcategory, parent: category, participatory_space: participatory_process) }
   let!(:user) { create :user, :confirmed, organization: organization }
+  let(:active_step_id) { component.participatory_space.active_step.id }
   let(:settings) do
     {
       menu_amendments: show_amendments,
       menu_meetings: show_meetings
     }
   end
+
+  let(:default_step_settings) do
+    {
+      show_not_answered: show_not_answered,
+      show_evaluating: show_evaluating,
+      show_accepted: show_accepted,
+      show_rejected: show_rejected,
+      show_withdrawn: show_withdrawn
+    }
+  end
+
   let(:show_amendments) { true }
   let(:show_meetings) { true }
+  let(:map_config) do
+    {
+      provider: :here,
+      api_key: "foo",
+      static: { url: "https://image.maps.ls.hereapi.com/mia/1.6/mapview" }
+    }
+  end
 
   before do
-    visit_component
+    allow(Decidim.config).to receive(:maps).and_return(map_config)
   end
 
   it "shows the map" do
+    visit_component
     within ".wrapper" do
       expect(page).to have_selector(".awesome-map")
       expect(page).to have_selector("#awesome-map")
@@ -44,6 +68,7 @@ describe "Show awesome map", type: :system do
   end
 
   it "has AwesomeMap javascript and CSS" do
+    visit_component
     within "head", visible: :all do
       expect(page).to have_xpath("//link[@rel='stylesheet'][contains(@href,'decidim_decidim_awesome_map')]", visible: :all)
       expect(page).to have_xpath("//link[@rel='stylesheet'][contains(@href,'decidim_map')]", visible: :all)
@@ -55,9 +80,58 @@ describe "Show awesome map", type: :system do
   end
 
   it "shows categories and colors" do
+    visit_component
     within ".awesome-map" do
       expect(page.body).to have_content(".awesome_map-category_#{category.id}")
       expect(page.body).to have_content(".awesome_map-category_#{subcategory.id}")
+    end
+  end
+
+  context "when step settings are as default" do
+    let(:show_accepted) { true }
+    let(:show_evaluating) { true }
+    let(:show_rejected) { false }
+    let(:show_withdrawn) { false }
+
+    before do
+      component.update!(step_settings: { active_step_id => {
+                          show_accepted: show_accepted,
+                          show_evaluating: show_evaluating,
+                          show_rejected: show_rejected,
+                          show_withdrawn: show_withdrawn
+                        } })
+      visit_component
+    end
+
+    it "shows accepted and evaluating proposals markers" do
+      sleep(4)
+      expect(page.body).to have_selector("div[title='#{accepted_proposal.title["en"]}']")
+      expect(page.body).to have_selector("div[title='#{evaluating_proposal.title["en"]}']")
+      expect(page.body).not_to have_selector("div[title='#{rejected_proposal.title["en"]}']")
+      expect(page.body).not_to have_selector("div[title='#{withdrawn_proposal.title["en"]}']")
+    end
+  end
+
+  context "when rejected and withdrawn proposal can be shown" do
+    let(:show_accepted) { true }
+    let(:show_evaluating) { true }
+    let(:show_rejected) { true }
+    let(:show_withdrawn) { true }
+
+    before do
+      component.update!(step_settings: { active_step_id => {
+                          show_accepted: show_accepted,
+                          show_evaluating: show_evaluating,
+                          show_rejected: show_rejected,
+                          show_withdrawn: show_withdrawn
+                        } })
+      visit_component
+    end
+
+    it "shows the rejected proposal" do
+      sleep(4)
+      expect(page.body).to have_selector("div[title='#{rejected_proposal.title["en"]}']")
+      expect(page.body).to have_selector("div[title='#{withdrawn_proposal.title["en"]}']")
     end
   end
 
