391cbf2e3ccd4dd13daabf71451158f91b17e532
Alejandro Puerta <71900287+apuertadelaguila@users.noreply.github.com>
2023-07-10 14:50:36 +0200

    Proposals displayed on Awesome Map (#223)
    * The js check now the awesome_map.config.show and the proposal's state
    
    * fix linter errors
    
    * revert package.json amd eslint changes
    
    * tests added for new functionality
    
    * fix: test refactor
    
    * fix: separate markers on map
    
    * fix: fix test, breakpoint
    
    ---------
    
    Co-authored-by: Marina Muñoz <marina.munoz@nazaries.com>

--- spec/system/awesome_map_spec.rb
+++ spec/system/awesome_map_spec.rb
@@ -10,20 +10,48 @@ describe "Show awesome map", type: :system do
   let!(:proposal) { create(:proposal, component: proposal_component, latitude: 40, longitude: 2) }
   let(:emendation) { build(:proposal, component: proposal_component, latitude: 42, longitude: 4) }
   let!(:proposal_amendment) { create(:proposal_amendment, amendable: proposal, emendation: emendation) }
-
+  let!(:accepted_proposal) { create(:proposal, :accepted, component: proposal_component, latitude: 40, longitude: -50) }
+  let!(:evaluating_proposal) { create(:proposal, :evaluating, component: proposal_component, latitude: 30, longitude: 45) }
+  let!(:not_answered_proposal) { create(:proposal, :not_answered, component: proposal_component, latitude: 70, longitude: 6) }
+  let!(:withdrawn_proposal) { create(:proposal, :withdrawn, component: proposal_component, latitude: 60, longitude: -30) }
+  let!(:rejected_proposal) { create(:proposal, :rejected, component: proposal_component, latitude: 10, longitude: 80) }
   let!(:category) { create(:category, participatory_space: participatory_process) }
   let!(:subcategory) { create(:subcategory, parent: category, participatory_space: participatory_process) }
   let!(:user) { create :user, :confirmed, organization: organization }
+  let(:active_step_id) { component.participatory_space.active_step.id }
   let(:settings) do
     {
       menu_amendments: show_amendments,
       menu_meetings: show_meetings
     }
   end
+
+  let(:step_settings) do
+    {
+      show_accepted: show_accepted,
+      show_evaluating: show_evaluating,
+      show_rejected: show_rejected,
+      show_withdrawn: show_withdrawn
+    }
+  end
+  let(:show_accepted) { true }
+  let(:show_evaluating) { true }
+  let(:show_rejected) { true }
+  let(:show_withdrawn) { true }
+
   let(:show_amendments) { true }
   let(:show_meetings) { true }
+  let(:map_config) do
+    {
+      provider: :here,
+      api_key: "foo",
+      static: { url: "https://image.maps.ls.hereapi.com/mia/1.6/mapview" }
+    }
+  end
 
   before do
+    allow(Decidim.config).to receive(:maps).and_return(map_config)
+    component.update!(step_settings: { active_step_id => step_settings })
     visit_component
   end
 
@@ -58,6 +86,31 @@ describe "Show awesome map", type: :system do
     within ".awesome-map" do
       expect(page.body).to have_content(".awesome_map-category_#{category.id}")
       expect(page.body).to have_content(".awesome_map-category_#{subcategory.id}")
+    end
+  end
+
+  context "when step settings are all true" do
+    it "shows all proposals markers" do
+      sleep(1)
+      expect(page.body).to have_selector("div[title='#{accepted_proposal.title["en"]}']")
+      expect(page.body).to have_selector("div[title='#{evaluating_proposal.title["en"]}']")
+      expect(page.body).to have_selector("div[title='#{rejected_proposal.title["en"]}']")
+      expect(page.body).to have_selector("div[title='#{withdrawn_proposal.title["en"]}']")
+    end
+  end
+
+  context "when step settings are all false" do
+    let(:show_accepted) { false }
+    let(:show_evaluating) { false }
+    let(:show_rejected) { false }
+    let(:show_withdrawn) { false }
+
+    it "does not show any proposal" do
+      sleep(1)
+      expect(page.body).not_to have_selector("div[title='#{accepted_proposal.title["en"]}']")
+      expect(page.body).not_to have_selector("div[title='#{evaluating_proposal.title["en"]}']")
+      expect(page.body).not_to have_selector("div[title='#{rejected_proposal.title["en"]}']")
+      expect(page.body).not_to have_selector("div[title='#{withdrawn_proposal.title["en"]}']")
     end
   end
 
