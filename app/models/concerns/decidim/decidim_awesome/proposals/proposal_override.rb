08938a32b4cf2f56486eb1bd35361b43bb8ebffc
Anna Topalidi <t.opalidisanna@gmail.com>
2023-08-31 22:53:14 +0200

    fix

--- app/models/concerns/decidim/decidim_awesome/proposals/proposal_override.rb
+++ app/models/concerns/decidim/decidim_awesome/proposals/proposal_override.rb
@@ -9,37 +9,53 @@ module Decidim
         included do
           def editable_by?(user)
             return true if draft? && created_by?(user)
-            return false unless default_edit_permissions(user)
 
-            allow_to_edit_proposals_after_import ? awesome_config_allows_editing? : true
+            if allow_to_edit_proposals_after_import.value && copied_from_other_component?
+              can_edit_copied_component?(user)
+            else
+              default_edit_permissions(user)
+            end
           end
 
           private
 
-          def default_edit_permissions(user)
-            return true if !published_state? && within_edit_time_limit? &&
-                           !copied_from_other_component? && created_by?(user)
+          def can_edit_copied_component?(user)
+            return false unless within_edit_time_limit? && created_by?(user)
 
-            false
+            awesome_config_allows_editing?
+          end
+
+          def default_edit_permissions(user)
+            !published_state? && within_edit_time_limit? && !copied_from_other_component? && created_by?(user)
           end
 
           def awesome_config_allows_editing?
-            awesome_config = allow_to_edit_proposals_after_import
+            return true if allow_to_edit_proposals_after_import.constraints.blank?
 
-            return true if awesome_config.constraints.blank?
-
-            constraints_to_check = manifest_to_check(awesome_config)
-
-            participatory_space.manifest.name.to_s == constraints_to_check
+            allow_to_edit_proposals_after_import.constraints.any? do |constraint|
+              check_constraint(constraint)
+            end
           end
 
           def allow_to_edit_proposals_after_import
             Decidim::DecidimAwesome::AwesomeConfig.find_or_initialize_by(var: :allow_to_edit_proposals_after_import)
           end
 
-          def manifest_to_check(awesome_config)
-            constraint = awesome_config.constraints.detect { |c| c.settings["participatory_space_manifest"] }
-            constraint&.settings ? constraint.settings["participatory_space_manifest"] : nil
+          def check_constraint(constraint)
+            constraint.settings.all? do |key, value|
+              case key
+              when "participatory_space_manifest"
+                value == participatory_space.manifest.name.to_s
+              when "participatory_space_slug"
+                value == participatory_space.slug
+              when "component_manifest"
+                value == component.manifest.name.to_s
+              when "component_id"
+                value == component.id
+              else
+                false
+              end
+            end
           end
         end
       end
--- spec/system/edit_proposal_after_import_spec.rb
+++ spec/system/edit_proposal_after_import_spec.rb
@@ -7,54 +7,91 @@ describe "Edit proposals after import", type: :system do
   let(:organization) { create :organization }
   let(:user) { create :user, :confirmed, organization: organization }
   let(:manifest_name) { "proposals" }
+  let!(:component) { create(:proposal_component, manifest: manifest, participatory_space: participatory_process) }
   let!(:proposal) { create(:proposal, users: [user], component: component) }
   let(:proposal_title) { translated(proposal.title) }
-  let!(:assembly) { create(:assembly, organization: organization) }
-  let!(:assembly_proposal_component) { create :component, participatory_space: assembly, manifest: manifest }
-  let!(:assembly_proposal) { create(:proposal, :published, :official, component: assembly_proposal_component, users: [user]) }
   let!(:allow_to_edit_proposals_after_import) { create(:awesome_config, organization: organization, var: :allow_to_edit_proposals_after_import, value: edit_proposal) }
-  let(:edit_proposal) { true }
+  let(:copied_component) { create(:proposal_component, manifest: manifest, participatory_space: participatory_process) }
+  let!(:copied_proposal) { create :proposal, component: copied_component, users: [user] }
 
   context "when editing proposal after import is enabled" do
+    let(:edit_proposal) { true }
+
     before do
       switch_to_host(organization.host)
       login_as user, scope: :user
-      assembly_proposal.coauthorships.first.update!(author: user)
-      visit decidim_assemblies.decidim_assembly_proposals_path(assembly_slug: assembly.slug, component_id: assembly_proposal_component.id)
+      proposal.coauthorships.first.update!(author: user)
+      copied_proposal.link_resources([proposal], "copied_from_component")
+      visit_component
     end
 
     context "when constrains are not present" do
       it "allows editing the proposal" do
-        click_link assembly_proposal.title["en"]
+        click_link proposal_title
         expect(page).to have_content("EDIT PROPOSAL")
       end
     end
 
     context "when constrains are present" do
       let!(:constraint) { create(:config_constraint, awesome_config: allow_to_edit_proposals_after_import, settings: settings) }
-      let(:settings) do
-        { "participatory_space_manifest" => "participatory_processes" }
-      end
 
       context "when participatory space is not the same" do
+        let(:settings) do
+          { "participatory_space_manifest" => "assemblies" }
+        end
+
         it "does not allows editing the proposal" do
-          puts "allow_to_edit_proposals_after_import #{allow_to_edit_proposals_after_import.inspect}"
-          puts "allow_to_edit_proposals_after_import.constraints #{allow_to_edit_proposals_after_import.constraints.inspect}"
-          click_link assembly_proposal.title["en"]
+          click_link proposal_title
           expect(page).not_to have_content("EDIT PROPOSAL")
         end
       end
 
       context "when participatory space is the same" do
-        before do
-          visit_component
-          click_link proposal_title
+        let(:settings) do
+          { "participatory_space_manifest" => "participatory_processes" }
         end
 
         it "allows editing the proposal" do
+          click_link proposal_title
           expect(page).to have_content("EDIT PROPOSAL")
         end
       end
+    end
+  end
+
+  context "when editing a proposal after import is disabled" do
+    let(:edit_proposal) { false }
+
+    before do
+      switch_to_host(organization.host)
+      login_as user, scope: :user
+      proposal.coauthorships.first.update!(author: user)
+      copied_proposal.link_resources([proposal], "copied_from_component")
+      allow_to_edit_proposals_after_import.reload
+      visit_component
+    end
+
+    it "does not allow editing the proposal" do
+      click_link proposal_title
+      expect(page).not_to have_content("EDIT PROPOSAL")
+    end
+  end
+
+  context "when editing a proposal after import by another user" do
+    let(:another_user) { create :user, :confirmed, organization: organization }
+    let(:edit_proposal) { true }
+
+    before do
+      switch_to_host(organization.host)
+      login_as another_user, scope: :user
+      proposal.coauthorships.first.update!(author: user)
+      copied_proposal.link_resources([proposal], "copied_from_component")
+      visit_component
+    end
+
+    it "does not allow editing the proposal" do
+      click_link proposal_title
+      expect(page).not_to have_content("EDIT PROPOSAL")
     end
   end
 end
