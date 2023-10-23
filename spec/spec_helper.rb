eeac7accdfa3ccd87551fcf1b716e3e24bcea020
Ivan Vergés <ivan@pokecode.net>
2023-10-23 13:55:05 +0200

    update codecov report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,9 @@
 
 require "decidim/dev"
 
-require "simplecov"
-SimpleCov.start "rails"
-if ENV["CODECOV"]
-  require "codecov"
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 
-Decidim::Dev.dummy_app_path =
-  File.expand_path(File.join(__dir__, "decidim_dummy_app"))
+Decidim::Dev.dummy_app_path = File.expand_path(File.join(__dir__, "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
 
