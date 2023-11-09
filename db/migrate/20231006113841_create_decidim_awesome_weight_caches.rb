d1a6b5c485869e0336cd6062e825ba3f467ffb0d
Ivan Vergés <ivan@pokecode.net>
2023-11-09 12:42:09 +0100

    migration name

--- db/migrate/20231006113841_create_decidim_awesome_weight_caches.rb
+++ db/migrate/20231006113841_create_decidim_awesome_weight_caches.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class CreateDecidimAwesomeWeightCaches < ActiveRecord::Migration[6.0]
+class CreateDecidimAwesomeProposalExtraFields < ActiveRecord::Migration[6.0]
   def change
     create_table :decidim_awesome_proposal_extra_fields do |t|
       # this might be polymorphic in the future (if other types of votes are supported)
-      t.references :decidim_proposal, null: false, index: { name: "decidim_awesome_proposals_weights_cache" }
+      t.references :decidim_proposal, null: false, index: { name: "decidim_awesome_proposal_extra_fields" }
 
       t.jsonb :vote_weights_totals
       t.integer :weight_total, default: 0
