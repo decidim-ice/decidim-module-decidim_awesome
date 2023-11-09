d613ae4a4aef17ebdcd8d460c6ecedb73056d4fc
Ivan Vergés <ivan@pokecode.net>
2023-11-09 12:58:15 +0100

    index name

--- db/migrate/20231006113841_create_decidim_awesome_proposal_extra_fields.rb
+++ db/migrate/20231006113841_create_decidim_awesome_proposal_extra_fields.rb
@@ -4,7 +4,7 @@ class CreateDecidimAwesomeProposalExtraFields < ActiveRecord::Migration[6.0]
   def change
     create_table :decidim_awesome_proposal_extra_fields do |t|
       # this might be polymorphic in the future (if other types of votes are supported)
-      t.references :decidim_proposal, null: false, index: { name: "decidim_awesome_proposal_extra_fields" }
+      t.references :decidim_proposal, null: false, index: { name: "decidim_awesome_extra_fields_on_proposal"} }
 
       t.jsonb :vote_weights_totals
       t.integer :weight_total, default: 0
