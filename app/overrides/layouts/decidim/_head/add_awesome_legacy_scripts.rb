c7bd689ffa8efe653d7e80036bec4b59a2f5ec6a
Ivan Vergés <ivan@pokecode.net>
2023-11-15 15:42:50 +0100

    avoid override _application.html.erb

--- app/overrides/layouts/decidim/_head/add_awesome_legacy_scripts.rb
+++ app/overrides/layouts/decidim/_head/add_awesome_legacy_scripts.rb
@@ -7,5 +7,8 @@ Deface::Override.new(virtual_path: "layouts/decidim/_head",
   '
 	<%= javascript_pack_tag "decidim_decidim_awesome", defer: false %>
 	<%= javascript_pack_tag("decidim_decidim_awesome_custom_fields") if Decidim::DecidimAwesome.enabled?(:proposal_custom_fields) %>
+  <% if show_public_intergram? %>
+    <%= render partial: "layouts/decidim/decidim_awesome/intergram_widget", locals: { settings: organization_awesome_config[:intergram_for_public_settings] } %>
+  <% end %>
 	'
 end
