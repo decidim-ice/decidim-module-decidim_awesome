797ed99a2b01173459b981c9ddc73f4a9922d567
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-11-17 22:34:21 +0100

    Replace stylesheet_pack_tag and jasvascript_pack_tag prefixing with append_

--- app/overrides/layouts/decidim/_head/add_awesome_legacy_scripts.rb
+++ app/overrides/layouts/decidim/_head/add_awesome_legacy_scripts.rb
@@ -5,8 +5,8 @@ Deface::Override.new(virtual_path: "layouts/decidim/_head",
                      insert_after: 'erb[loud]:contains(\'javascript_pack_tag "decidim_core"\')') do
   # NOTE THAT THIS ONLY APPLIES TO DECIDIM 0.26. This will be ignored in 0.27
   '
-	<%= javascript_pack_tag "decidim_decidim_awesome", defer: false %>
-	<%= javascript_pack_tag("decidim_decidim_awesome_custom_fields") if Decidim::DecidimAwesome.enabled?(:proposal_custom_fields) %>
+	<%= append_javascript_pack_tag "decidim_decidim_awesome", defer: false %>
+	<%= append_javascript_pack_tag("decidim_decidim_awesome_custom_fields") if Decidim::DecidimAwesome.enabled?(:proposal_custom_fields) %>
   <% if show_public_intergram? %>
     <%= render partial: "layouts/decidim/decidim_awesome/intergram_widget", locals: { settings: organization_awesome_config[:intergram_for_public_settings] } %>
   <% end %>
