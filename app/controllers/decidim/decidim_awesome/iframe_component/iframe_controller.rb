0208180221824e41ebc27013cecafc7759607a04
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-11-17 18:41:51 +0100

    Set content security policies on iframes controller show action

--- app/controllers/decidim/decidim_awesome/iframe_component/iframe_controller.rb
+++ app/controllers/decidim/decidim_awesome/iframe_component/iframe_controller.rb
@@ -6,6 +6,7 @@ module Decidim
       class IframeController < DecidimAwesome::BlankComponentController
         ALLOWED_ATTRIBUTES = %w(src width height frameborder title allow allowpaymentrequest name referrerpolicy sandbox srcdoc allowfullscreen).freeze
         helper_method :iframe, :remove_margins?, :viewport_width?
+        before_action :add_additional_csp_directives, only: :show
 
         def show; end
 
@@ -26,6 +27,15 @@ module Decidim
 
         def viewport_width?
           current_component.settings.viewport_width
+        end
+
+        def add_additional_csp_directives
+          iframe_urls = Nokogiri::HTML::DocumentFragment.parse(iframe).children.select { |x| x.name == "iframe" }.map { |x| x.attribute("src").value }
+          return if iframe_urls.blank?
+
+          iframe_urls.each do |url|
+            content_security_policy.append_csp_directive("frame-src", url)
+          end
         end
       end
     end
