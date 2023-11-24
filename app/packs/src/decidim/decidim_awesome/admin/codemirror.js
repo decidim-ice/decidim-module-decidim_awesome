f79afe7006ddee9bf4b41942607c96148e0a93f9
Hugoren Martinako <aumpfbahn@gmail.com>
2023-11-24 12:30:44 +0100

    restore codemirror

--- app/packs/src/decidim/decidim_awesome/admin/codemirror.js
+++ app/packs/src/decidim/decidim_awesome/admin/codemirror.js
@@ -1,7 +1,7 @@
 import CodeMirror from "codemirror"
 import "codemirror/mode/css/css"
 import "codemirror/keymap/sublime"
-import "stylesheets/decidim/decidim_awesome/admin/codemirror.scss";
+import "codemirror/lib/codemirror.css";
 
 $(() => {
   $(".awesome-edit-config .scoped_styles_container textarea").each((_idx, el) => {
