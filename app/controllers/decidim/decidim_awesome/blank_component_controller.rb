e8857cda6125b1ba87757fe297dbaea1d06881b8
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-11-28 10:52:37 +0100

    Use current_component in blank_component_controller

--- app/controllers/decidim/decidim_awesome/blank_component_controller.rb
+++ app/controllers/decidim/decidim_awesome/blank_component_controller.rb
@@ -6,7 +6,7 @@ module Decidim
     class BlankComponentController < Decidim::Components::BaseController
       # just redirects to settings
       def settings
-        redirect_to EngineRouter.admin_proxy(component.participatory_space).edit_component_path(id: component)
+        redirect_to EngineRouter.admin_proxy(current_component.participatory_space).edit_component_path(id: current_component)
       end
 
       private
@@ -14,14 +14,10 @@ module Decidim
       def set_component_breadcrumb_item
         context_breadcrumb_items << {
           label: current_component.name,
-          url: EngineRouter.admin_proxy(component.participatory_space).edit_component_path(id: component),
+          url: EngineRouter.admin_proxy(current_component.participatory_space).edit_component_path(id: current_component),
           active: false,
           resource: current_component
         }
-      end
-
-      def component
-        Decidim::Component.find(params[:component_id])
       end
     end
   end
