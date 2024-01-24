13fe18e91711b61ec79a92c5e2efd7d80276e566
Ivan Vergés <ivan@pokecode.net>
2024-01-24 19:04:50 +0100

    add modal explanation window

---
+++ app/packs/src/decidim/decidim_awesome/amendments/show_modal_on_limits.js
@@ -0,0 +1,25 @@
+$(() => {
+  const $modal = $("#LimitAmendmentsModal");
+  if ($modal.length === 0 || $(".sign-out-link").length === 0) {
+    return;
+  }
+
+  const showModal = () => {
+    if ($modal.is(":visible")) {
+      return false;
+    }
+    return true;
+  };
+
+  $modal.find("a").on("click", () => {
+    $modal.foundation("close");
+  });
+
+  $(".card__amend-button").on("click", ".amend_button_card_cell", (evt) => {
+    if (showModal())  {
+      evt.preventDefault();
+      evt.stopPropagation();
+      $modal.foundation("open");
+    }
+  });
+});
