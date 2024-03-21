17e19f1385438540df421f442ed1630f4c0a8946
elviabth <ejbenedith@gmail.com>
2024-03-21 10:39:18 +0100

    run bundle exec erblint app/**/*.erb

--- app/packs/src/decidim/decidim_awesome/awesome_map/api/proposals_fetcher.js
+++ app/packs/src/decidim/decidim_awesome/awesome_map/api/proposals_fetcher.js
@@ -24,6 +24,7 @@ export default class ProposalsFetcher extends Fetcher {
                     }
                   }
                   author {
+                    id
                     name
                   }
                   body {
