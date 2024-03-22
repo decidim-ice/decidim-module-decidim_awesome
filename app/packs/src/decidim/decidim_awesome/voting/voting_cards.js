e324a2d3e98a61eab5aa0fcc881529e0128097e5
elviabth <ejbenedith@gmail.com>
2024-03-22 12:19:31 +0100

    replace jQuery for js vanilla in voting_cards.js

--- app/packs/src/decidim/decidim_awesome/voting/voting_cards.js
+++ app/packs/src/decidim/decidim_awesome/voting/voting_cards.js
@@ -1,63 +1,65 @@
-$(() => {
-  if ($(".voting-voting_cards").length === 0 || $("#VotingCardsModalHelp").length === 0 || $(".sign-out-link").length === 0) {
+document.addEventListener("DOMContentLoaded", function () {
+  const votingCards = document.querySelector(".voting-voting_cards");
+  const modalHelp = document.getElementById("voting-cards-proposal-modal");
+  const signOutLink = document.querySelector(".sign-out-link");
+
+  if (!votingCards || !modalHelp || !signOutLink) {
     return;
   }
 
-  const $modal = $("#VotingCardsModalHelp");
-  const $card = $modal.find(".current-choice .vote-card");
-  const $check = $("#voting_cards-skip_help");
+  const modal = document.getElementById("voting-cards-proposal-modal");
+  const card = modal.querySelector(".current-choice .vote-card");
+  const check = document.getElementById("voting_cards-skip_help");
 
   const storage = () => {
-    return JSON.parse(localStorage.getItem("hideTreeFlagsModalHelp") || "{}")
+    return JSON.parse(localStorage.getItem("hideTreeFlagsModalHelp") || "{}");
   };
 
   const isChecked = () => {
-    return storage()[$check.val()];
+    return storage()[check.value];
   };
 
   const saveState = (val) => {
     const show = storage();
-    show[$check.val()] = val;
-    localStorage.setItem("hideTreeFlagsModalHelp", JSON.stringify(show))
+    show[check.value] = val;
+    localStorage.setItem("hideTreeFlagsModalHelp", JSON.stringify(show));
   };
 
   const showModal = () => {
-    if (isChecked()) {
-      return false;
-    }
-    if ($modal.is(":visible")) {
+    if (isChecked() || modal.style.display === "block") {
       return false;
     }
     return true;
   };
 
-  $check.on("change", () => {
-    saveState($check.is(":checked"))
+  check.addEventListener("change", () => {
+    saveState(check.checked);
   });
 
-  $modal.find(".vote-action").on("click", () => {
-    $modal.data("action").click();
-    $modal.foundation("close");
+  modal.querySelector(".vote-action").addEventListener("click", () => {
+    modal.dataset.action.click();
+    $(modal).foundation("close");
   });
 
-  $(".button--vote-button .voting-voting_cards").on("click", ".vote-action", (evt) => {
+  document.querySelector(".button--vote-button .voting-voting_cards").addEventListener("click", function (evt) {
     if (showModal()) {
       evt.stopPropagation();
       evt.preventDefault();
-      $check.prop("checked", isChecked());
-      $modal.data("action", evt.currentTarget);
-      $card[0].classList = evt.currentTarget.classList;
+      check.checked = isChecked();
+      modal.dataset.action = evt.currentTarget;
+      card.className = evt.currentTarget.className;
       if (evt.currentTarget.children.length > 1) {
-        $card.html(`${evt.currentTarget.children[1].outerHTML}<p class="vote-label">${evt.currentTarget.children[1].children[0].textContent}</p>`);
-      } else if ($card[0].classList.contains("button")) {
-        $card[0].classList.remove("button");
-        $card.html(`<p class="vote-label">${evt.currentTarget.title}</p>`);
+        card.innerHTML = `${evt.currentTarget.children[1].outerHTML}<p class="vote-label">${evt.currentTarget.children[1].children[0].textContent}</p>`;
+      } else if (card.classList.contains("button")) {
+        card.classList.remove("button");
+        card.innerHTML = `<p class="vote-label">${evt.currentTarget.title}</p>`;
       } else {
-        $card.html(`<p class="vote-label">${evt.currentTarget.textContent}</p>`);
+        card.innerHTML = `<p class="vote-label">${evt.currentTarget.textContent}</p>`;
       }
-      $modal.foundation("open");
+      $(modal).foundation("open");
     } else {
-      $(evt.currentTarget).closest(".voting-voting_cards").addClass("loading");
+      evt.currentTarget.closest(".voting-voting_cards").classList.add("loading");
     }
   });
 });
+
