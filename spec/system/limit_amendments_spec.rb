16624e78362d53d55f6870aae44e610df0726595
Anna Topalidi <t.opalidisanna@gmail.com>
2024-05-09 10:49:30 +0200

    fix rubocop

--- spec/system/limit_amendments_spec.rb
+++ spec/system/limit_amendments_spec.rb
@@ -2,24 +2,24 @@
 
 require "spec_helper"
 
-describe "Custom proposals fields", type: :system do
+describe "Custom proposals fields" do
   include_context "with a component"
   let(:manifest_name) { "proposals" }
   let(:component) do
     create(:proposal_component,
-           manifest: manifest,
-           participatory_space: participatory_process, settings: settings, step_settings: step_settings)
+           manifest:,
+           participatory_space: participatory_process, settings:, step_settings:)
   end
   let(:active_step_id) { participatory_process.active_step.id }
-  let(:step_settings) { { active_step_id => { amendment_creation_enabled: amendment_creation_enabled, amendments_visibility: visibility } } }
+  let(:step_settings) { { active_step_id => { amendment_creation_enabled:, amendments_visibility: visibility } } }
   let(:visibility) { "all" }
-  let(:settings) { { amendments_enabled: amendments_enabled, limit_pending_amendments: limit_pending_amendments } }
+  let(:settings) { { amendments_enabled:, limit_pending_amendments: } }
   let(:user) { create(:user, :confirmed, organization: component.organization) }
 
-  let!(:proposal) { create :proposal, component: component }
-  let!(:emendation) { create(:proposal, title: { en: "An emendation" }, component: component) }
-  let!(:amendment) { create(:amendment, amendable: proposal, emendation: emendation, state: amendment_state) }
-  let!(:hidden_emendation) { create(:proposal, :hidden, title: { en: "A stupid emendation" }, component: component) }
+  let!(:proposal) { create(:proposal, component:) }
+  let!(:emendation) { create(:proposal, title: { en: "An emendation" }, component:) }
+  let!(:amendment) { create(:amendment, amendable: proposal, emendation:, state: amendment_state) }
+  let!(:hidden_emendation) { create(:proposal, :hidden, title: { en: "A stupid emendation" }, component:) }
   let!(:hidden_amendment) { create(:amendment, amendable: proposal, emendation: hidden_emendation, state: "evaluating") }
 
   let(:amendment_state) { "evaluating" }
@@ -30,7 +30,7 @@ describe "Custom proposals fields", type: :system do
   before do
     login_as user, scope: :user
     visit_component
-    click_link proposal.title["en"]
+    click_link_or_button proposal.title["en"]
   end
 
   def amendment_path
@@ -41,7 +41,7 @@ describe "Custom proposals fields", type: :system do
     it "cannot create a new one" do
       expect(page).to have_content(proposal.title["en"])
       expect(page).to have_content(emendation.title["en"])
-      click_link "Amend"
+      click_link_or_button "Amend"
 
       within ".limit_amendments_modal" do
         expect(page).to have_link(href: amendment_path)
@@ -55,25 +55,25 @@ describe "Custom proposals fields", type: :system do
       it "can create a new one" do
         expect(page).to have_content(proposal.title["en"])
         expect(page).to have_content(emendation.title["en"])
-        click_link "Amend"
+        click_link_or_button "Amend"
 
-        expect(page).not_to have_content("Currently, there's another amendment being evaluated for this proposal.")
-        expect(page).not_to have_content(proposal.title["en"])
+        expect(page).to have_no_content("Currently, there's another amendment being evaluated for this proposal.")
+        expect(page).to have_no_content(proposal.title["en"])
         expect(page).to have_content("CREATE AMENDMENT DRAFT")
       end
     end
   end
 
   context "when there's no pending amendments" do
-    let!(:amendment) { create(:amendment, emendation: emendation, state: amendment_state) }
+    let!(:amendment) { create(:amendment, emendation:, state: amendment_state) }
 
     it "can create a new one" do
       expect(page).to have_content(proposal.title["en"])
-      expect(page).not_to have_content(emendation.title["en"])
-      click_link "Amend"
+      expect(page).to have_no_content(emendation.title["en"])
+      click_link_or_button "Amend"
 
-      expect(page).not_to have_content("Currently, there's another amendment being evaluated for this proposal.")
-      expect(page).not_to have_content(proposal.title["en"])
+      expect(page).to have_no_content("Currently, there's another amendment being evaluated for this proposal.")
+      expect(page).to have_no_content(proposal.title["en"])
       expect(page).to have_content("CREATE AMENDMENT DRAFT")
     end
   end
@@ -84,10 +84,10 @@ describe "Custom proposals fields", type: :system do
     it "can create a new one" do
       expect(page).to have_content(proposal.title["en"])
       expect(page).to have_content(emendation.title["en"])
-      click_link "Amend"
+      click_link_or_button "Amend"
 
-      expect(page).not_to have_content("Currently, there's another amendment being evaluated for this proposal.")
-      expect(page).not_to have_content(proposal.title["en"])
+      expect(page).to have_no_content("Currently, there's another amendment being evaluated for this proposal.")
+      expect(page).to have_no_content(proposal.title["en"])
       expect(page).to have_content("CREATE AMENDMENT DRAFT")
     end
   end
@@ -97,11 +97,11 @@ describe "Custom proposals fields", type: :system do
 
     it "cannot create a new one" do
       expect(page).to have_content(proposal.title["en"])
-      expect(page).not_to have_content(emendation.title["en"])
-      click_link "Amend"
+      expect(page).to have_no_content(emendation.title["en"])
+      click_link_or_button "Amend"
 
       within ".limit_amendments_modal" do
-        expect(page).not_to have_link(href: amendment_path)
+        expect(page).to have_no_link(href: amendment_path)
         expect(page).to have_content("Currently, there's another amendment being evaluated for this proposal.")
       end
     end
