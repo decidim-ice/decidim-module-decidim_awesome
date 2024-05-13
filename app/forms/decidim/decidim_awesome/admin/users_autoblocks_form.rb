d216156e93ac2cb3927b57caa3acbe21d4ee2c29
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-05-13 16:35:36 +0200

    Change edition of rules to use allow and block lists

--- app/forms/decidim/decidim_awesome/admin/users_autoblocks_form.rb
+++ app/forms/decidim/decidim_awesome/admin/users_autoblocks_form.rb
@@ -6,20 +6,21 @@ module Decidim
       class UsersAutoblocksForm < Decidim::Form
         attribute :type, String
         attribute :weight, Integer, default: "1"
-        attribute :variable, String
-        attribute :blocklist, Boolean, default: true
+        attribute :allowlist, String
+        attribute :blocklist, String
+        attribute :block_if_detected, Boolean, default: true
         attribute :enabled, Boolean, default: true
 
         validates :type, inclusion: { in: Decidim::DecidimAwesome::UserAutoblockScoresPresenter::USERS_AUTOBLOCKS_TYPES.keys }
         validates :weight, presence: true
-        validates :variable, presence: true, if: ->(form) { USERS_AUTOBLOCKS_TYPES[form.type].presence&.dig(:has_variable) }
 
         def to_params
           {
             type:,
             weight:,
-            variable:,
+            allowlist:,
             blocklist:,
+            block_if_detected:,
             enabled:
           }
         end
