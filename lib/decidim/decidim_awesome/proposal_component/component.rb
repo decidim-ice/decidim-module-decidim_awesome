2074bae392b4d748ede9653fafa77117849f29cd
Hadrien Froger <hadrien@octree.ch>
2024-05-15 02:30:25 +0100

    refactor: rename export name for explicit trad

--- lib/decidim/decidim_awesome/proposal_component/component.rb
+++ lib/decidim/decidim_awesome/proposal_component/component.rb
@@ -4,10 +4,10 @@
 # included in open-data to be able to export all private fields
 # from the administration without exposing data to the frontend.
 proposal_component = Decidim.find_component_manifest("proposals")
-proposal_component.exports :private_proposals do |exports|
+proposal_component.exports :awesome_private_proposals do |exports|
   exports.collection do |component_instance, user|
     space = component_instance.participatory_space
-
+    
     collection = Decidim::Proposals::Proposal
                  .published
                  .not_hidden
@@ -20,7 +20,7 @@ proposal_component.exports :private_proposals do |exports|
       collection
     end
   end
-
+  
   exports.include_in_open_data = false
   exports.serializer Decidim::DecidimAwesome::PrivateProposalSerializer
 end
