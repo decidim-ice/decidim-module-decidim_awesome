5dcb5ae7183d80f0808a5844fe76d6c051aa13c3
Anna Topalidi <t.opalidisanna@gmail.com>
2024-08-14 21:25:08 +0200

    fix spec javascript config vars

--- lib/decidim/decidim_awesome/test/shared_examples/config_examples.rb
+++ lib/decidim/decidim_awesome/test/shared_examples/config_examples.rb
@@ -4,7 +4,7 @@ shared_examples "javascript config vars" do
   it "has DecidimAwesome object" do
     expect(page.body).to have_content("window.DecidimAwesome")
     expect(page.body).to have_content("window.DecidimAwesome.version")
-    expect(page.body).to have_content("window.DecidimAwesome.editorUploaderPath")
+    expect(page.body).to have_content("window.DecidimAwesome.editor_uploader_path")
     expect(page.body).to have_content("window.DecidimAwesome.texts")
   end
 end
@@ -12,7 +12,7 @@ end
 shared_examples "has menu link" do |item|
   let(:prefix) { "config/" }
   it "shows the feature link" do
-    within ".sidebar-menu" do
+    within ".secondary-nav" do
       expect(page).to have_link(href: "/admin/decidim_awesome/#{prefix}#{item}")
     end
   end
@@ -21,14 +21,13 @@ end
 shared_examples "do not have menu link" do |item|
   let(:prefix) { "config/" }
   it "do not show the feature link" do
-    within ".sidebar-menu" do
+    within ".secondary-nav" do
       expect(page).not_to have_link(href: "/admin/decidim_awesome/#{prefix}#{item}")
     end
   end
 end
 
-shared_examples "forbids disabled feature without redirect" do
-  render_views
+shared_examples "forbids disabled feature" do
   let(:feature) { :menu }
   let(:features) { [feature] }
   before do
@@ -37,24 +36,7 @@ shared_examples "forbids disabled feature without redirect" do
     end
   end
 
-  it "fails with error" do
-    action
-
-    expect(response.body).to eq("no permissions for #{feature}")
-  end
-end
-
-shared_examples "forbids disabled feature with redirect" do
-  render_views
-  let(:feature) { :menu }
-  let(:features) { [feature] }
-  before do
-    features.each do |feat|
-      allow(Decidim::DecidimAwesome.config).to receive(feat).and_return(:disabled)
-    end
-  end
-
-  it "fails with error" do
+  it "redirects with error" do
     action
 
     expect(flash[:alert]).not_to be_empty
