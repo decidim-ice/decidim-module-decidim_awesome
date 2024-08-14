5466557215be2631ceb0efd591189746543bcadf
Anna Topalidi <t.opalidisanna@gmail.com>
2024-08-14 13:58:19 +0200

    fix edit proposal spec

--- lib/decidim/decidim_awesome/test/shared_examples/editor_examples.rb
+++ lib/decidim/decidim_awesome/test/shared_examples/editor_examples.rb
@@ -24,37 +24,47 @@ shared_examples "has video support" do
   end
 end
 
-shared_examples "has no drag and drop" do
-  it "cannot drop a file" do
+shared_examples "has no drag and drop" do |rte|
+  it "has no help text" do
     expect(page).not_to have_content("Add images by dragging & dropping or pasting them.")
-    find(editor_selector).drop(image)
-    expect(page.execute_script("return document.querySelector('#{editor_selector}').value")).not_to eq("[Uploading file...]")
-    sleep 1
-    last_image = Decidim::DecidimAwesome::EditorImage.last
-    expect(last_image).to be_nil
+  end
+
+  if rte
+    it "has image button" do
+      expect(page).not_to have_xpath("//button[@class='ql-image']")
+    end
+  else
+    it "has no paste event" do
+      sleep 1
+      expect(page.execute_script("return typeof $._data($('#{editor_selector}')[0], 'events').paste")).to eq("undefined")
+    end
+
+    it "has no drop event" do
+      sleep 1
+      expect(page.execute_script("return typeof $._data($('#{editor_selector}')[0], 'events').drop")).to eq("undefined")
+    end
   end
 end
 
-shared_examples "has drag and drop" do
-  it "can drop a file" do
-    within "form.edit_proposal" do
-      expect(page).to have_content("Add images by dragging & dropping or pasting them.")
-      fill_in :proposal_body, with: ""
+shared_examples "has drag and drop" do |rte|
+  it "has help text" do
+    expect(page).to have_content("Add images by dragging & dropping or pasting them.")
+  end
+
+  if rte
+    it "has image button" do
+      expect(page).to have_xpath("//button[@class='ql-image']")
     end
-    find(editor_selector).drop(image)
-    expect(page.execute_script("return document.querySelector('#{editor_selector}').value")).to include("[Uploading file...]")
-    sleep 1
-    last_image = Decidim::DecidimAwesome::EditorImage.last
-    expect(last_image).to be_present
-    content = page.execute_script("return document.querySelector('#{editor_selector}').value")
-    expect(content).to include(last_image.attached_uploader(:file).path)
-    within "form.edit_proposal" do
-      # ensures valid body validations
-      fill_in :proposal_body, with: "This is a super test with lots of downcases characters to be sure that the image name does not mess with percentage of caps.\n#{content}"
-      fill_in :proposal_title, with: "This is a test proposal"
-      click_on "Send"
+  else
+    it "has paste event" do
+      sleep 1
+      expect(page.execute_script("return typeof $._data($('#{editor_selector}')[0], 'events').paste")).to eq("object")
     end
-    expect(proposal.reload.body["en"]).to include(last_image.attached_uploader(:file).path)
+
+    it "has drop event" do
+      sleep 1
+      expect(page.execute_script("return typeof $._data($('#{editor_selector}')[0], 'events').drop")).to eq("object")
+    end
   end
 end
 
