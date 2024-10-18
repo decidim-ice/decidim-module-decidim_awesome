43afc89006448879c5b9c82806e7106fc80e6095
Ivan Vergés <ivan@pokecode.net>
2024-10-18 12:45:01 +0200

    rm file

--- app/packs/src/decidim/editor.js
+++
@@ -1,6 +0,0 @@
-import {awesomeCreateQuillEditor} from "src/decidim/decidim_awesome/editors/editor"
-
-console.log("APP OVERRIDE EDITOR")
-export default function createQuillEditor(container) {
-  return awesomeCreateQuillEditor(container);
-}
