8650916b7aadcd20e1b3a67073bad04d62a2abb9
Ivan Vergés <ivan@pokecode.net>
2024-11-05 12:13:59 +0100

    fix package.json

--- app/packs/src/decidim/decidim_awesome/admin/verification_selects.js
+++ app/packs/src/decidim/decidim_awesome/admin/verification_selects.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-new */
 
-import TomSelect from "tom-select/dist/cjs/tom-select.popular";
+// import TomSelect from "tom-select/dist/cjs/tom-select.popular";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const selectContainer = document.getElementById("config_force_authorization_after_login");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const selectContainer = document.getElementById("config_force_authorization_after_login");
 
-  if (!selectContainer) {
-    return;
-  }
+//   if (!selectContainer) {
+//     return;
+//   }
 
-  new TomSelect(selectContainer, {
-    plugins: ["remove_button", "dropdown_input"],
-    create: false,
-    render: {
-      option: function (data, escape) {
-        return `<div>${escape(data.text)}</div>`;
-      }
-    }
-  });
-});
+//   new TomSelect(selectContainer, {
+//     plugins: ["remove_button", "dropdown_input"],
+//     create: false,
+//     render: {
+//       option: function (data, escape) {
+//         return `<div>${escape(data.text)}</div>`;
+//       }
+//     }
+//   });
+// });
