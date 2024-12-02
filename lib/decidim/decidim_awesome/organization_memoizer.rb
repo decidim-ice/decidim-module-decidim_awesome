3775705a319c4b32b6e808c273c00f41ddbf3fac
Ivan Vergés <Ivan Vergs>
2024-12-02 20:39:42 +0100

    ensure request exits

--- lib/decidim/decidim_awesome/organization_memoizer.rb
+++ lib/decidim/decidim_awesome/organization_memoizer.rb
@@ -11,10 +11,10 @@ module Decidim
 
       # memoize a piece of code in the class instead of the instance (helper are initialized for each view)
       # only works if request.env["decidim.current_organization"] is defined
-      def memoize(key, &block)
-        return yield unless request.env["decidim.current_organization"]&.id
+      def memoize(key, &)
+        return yield unless defined?(request) && request.env["decidim.current_organization"]&.id
 
-        OrganizationMemoizer.memoize("#{request.env["decidim.current_organization"].id}-#{key}", &block)
+        OrganizationMemoizer.memoize("#{request.env["decidim.current_organization"].id}-#{key}", &)
       end
     end
   end
