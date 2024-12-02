459702bd729f6079efb3e1a8901f035bbd36eafc
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-12-02 17:01:22 +0100

    Improve users autoblocks config form

---
+++ app/packs/src/decidim/decidim_awesome/admin/users_autoblocks.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const configCheckBox = document.getElementById("users_autoblocks_config_perform_block");
+
+  configCheckBox.addEventListener("change", (event) => {
+    const form = event.currentTarget.form;
+    const submitButton = form.querySelector("[data-perform-block-message]");
+    const messages = JSON.parse(submitButton.dataset.performBlockMessage);
+    const justificationMessage = form.querySelector("[data-justification-message]");
+
+    if (event.target.checked) {
+      submitButton.setAttribute("data-confirm", submitButton.dataset.confirmMessage);
+      justificationMessage.classList.remove("hidden");
+      justificationMessage.querySelector("input").required = true;
+    } else {
+      submitButton.removeAttribute("data-confirm");
+      justificationMessage.classList.add("hidden");
+      justificationMessage.querySelector("input").required = false;
+    }
+  });
+});
