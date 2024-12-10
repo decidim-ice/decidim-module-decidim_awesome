5ac9f0e4f8b04e03173be50418a40665142abbbd
Ivan Vergés <ivan@pokecode.net>
2024-12-10 09:45:33 +0100

    fix m cell

--- app/cells/concerns/decidim/decidim_awesome/proposal_m_cell_override.rb
+++ app/cells/concerns/decidim/decidim_awesome/proposal_m_cell_override.rb
@@ -9,8 +9,7 @@ module Decidim
         alias_method :decidim_original_cache_hash, :cache_hash
 
         def cache_hash
-          extra_hash = model.extra_fields&.reload&.vote_weight_totals
-          "#{decidim_original_cache_hash}#{Decidim.cache_key_separator}#{extra_hash}"
+          @cache_hash ||= "#{decidim_original_cache_hash}#{Decidim.cache_key_separator}#{model.extra_fields&.vote_weight_totals}"
         end
       end
     end
