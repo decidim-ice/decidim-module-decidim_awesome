9f4df8ff843600df5730a55f940c6b2f03c63967
Ivan Vergés <ivan@pokecode.net>
2025-03-11 13:19:37 +0100

    bump version (#373)

--- lib/decidim/decidim_awesome/version.rb
+++ lib/decidim/decidim_awesome/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-decidim_awesome version.
   module DecidimAwesome
-    VERSION = "0.11.3"
+    VERSION = "0.11.4"
     COMPAT_DECIDIM_VERSION = [">= 0.28.0", "< 0.29"].freeze
   end
 end
