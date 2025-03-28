f7c2129c5081571123ad6515fc54c30a78456976
Ivan Vergés <ivan@pokecode.net>
2025-03-28 20:56:34 +0100

    fix proposal override

--- app/forms/concerns/decidim/decidim_awesome/proposals/proposal_form_override.rb
+++ app/forms/concerns/decidim/decidim_awesome/proposals/proposal_form_override.rb
@@ -10,6 +10,12 @@ module Decidim
           alias_method :decidim_original_map_model, :map_model
           attribute :private_body, Decidim::Attributes::RichText
 
+          if name == "Decidim::Proposals::ProposalForm"
+            # the original decidim uses CleanString, as no images are allowed for non admins
+            # The RichText attribute tranforms signed_id blobs to ObjectIDs
+            attribute :body, Decidim::Attributes::RichText
+          end
+
           def map_model(model)
             decidim_original_map_model(model)
             self.private_body = model.private_body
