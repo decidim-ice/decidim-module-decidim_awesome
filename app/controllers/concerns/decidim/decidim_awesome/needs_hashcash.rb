a90ea5c864e8995f6a862195e9462f74ab826477
David Igón <david@pokecode.net>
2025-07-08 16:37:20 +0200

    add hashcash hidden file in components controllers

--- app/controllers/concerns/decidim/decidim_awesome/needs_hashcash.rb
+++ app/controllers/concerns/decidim/decidim_awesome/needs_hashcash.rb
@@ -30,11 +30,13 @@ module Decidim
 
       # Dynamically configures the gem https://github.com/BaseSecrete/active_hashcash
       def set_hashcash_bits
-        if controller_name == "sessions"
-          ActiveHashcash.bits = awesome_hashcash_bits(:login)
-        elsif controller_name == "registrations"
-          ActiveHashcash.bits = awesome_hashcash_bits(:signup)
-        end
+        ActiveHashcash.bits = if controller_name == "sessions"
+                                awesome_hashcash_bits(:login)
+                              elsif controller_name == "registrations"
+                                awesome_hashcash_bits(:signup)
+                              else
+                                awesome_hashcash_bits(:login)
+                              end
       end
     end
   end
