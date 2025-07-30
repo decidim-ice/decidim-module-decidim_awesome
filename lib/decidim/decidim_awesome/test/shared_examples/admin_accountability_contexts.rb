05cf9aa7443fb523b559a96b06395dccf37648a8
Fran Bolívar <francisco.bolivar@nazaries.com>
2025-07-30 13:50:32 +0200

    lint

--- lib/decidim/decidim_awesome/test/shared_examples/admin_accountability_contexts.rb
+++ lib/decidim/decidim_awesome/test/shared_examples/admin_accountability_contexts.rb
@@ -7,5 +7,5 @@ shared_context "with admin accountability helpers" do
       fill_in_datepicker :q_created_at_lteq_date, with: end_date.strftime("%d/%m/%Y") if end_date.present?
       find("*[type=submit]").click
     end
-end
+  end
 end
