2f180c41a083a0b7090f12e4fbd28b0dad9ac1d2
David Igón <david@pokecode.net>
2025-07-31 17:48:34 +0200

    update to 0.12.4

--- lib/decidim/decidim_awesome/version.rb
+++ lib/decidim/decidim_awesome/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-decidim_awesome version.
   module DecidimAwesome
-    VERSION = "0.12.3"
+    VERSION = "0.12.4"
     COMPAT_DECIDIM_VERSION = [">= 0.29.1", "< 0.30"].freeze
   end
 end
