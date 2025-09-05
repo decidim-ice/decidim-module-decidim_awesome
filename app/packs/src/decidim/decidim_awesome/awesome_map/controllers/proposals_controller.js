26a2fec36b1668cc3d919b63180fb153556adac1
Alejandro Puerta <alejandro.puerta@nazaries.com>
2025-09-05 10:05:35 +0200

    lint js proposals_controller.js

--- app/packs/src/decidim/decidim_awesome/awesome_map/controllers/proposals_controller.js
+++ app/packs/src/decidim/decidim_awesome/awesome_map/controllers/proposals_controller.js
@@ -36,9 +36,7 @@ export default class ProposalsController extends Controller {
       // also assign parent's proposal taxonomies to it
       // console.log("onNode proposal", proposal, "amendment:", proposal.amendments)
       if (proposal.amendments && proposal.amendments.length) {
-        proposal.amendments
-          .filter(amendment => amendment && amendment.emendation)
-          .forEach((amendment) => {
+        proposal.amendments.filter((amendment) => amendment && amendment.emendation).forEach((amendment) => {
           this.amendments[amendment.emendation.id] = proposal;
         });
       }
