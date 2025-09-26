33f0c0538c9a201d8984c7ad5cde7043cb2611bd
Alejandro Puerta <71900287+apuertadelaguila@users.noreply.github.com>
2025-09-26 12:55:38 +0200

    Fix nil participatory_space error in SpaceConstraintFinder (#463)

---
+++ spec/system/admin/assembly_creation_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "Assembly creation with decidim_awesome constraints" do
+  let(:organization) { create(:organization) }
+  let!(:admin) { create(:user, :admin, :confirmed, organization:) }
+
+  before do
+    switch_to_host(organization.host)
+    login_as admin, scope: :user
+  end
+
+  context "when creating a new assembly" do
+    it "does not raise error when accessing assembly creation form" do
+      visit decidim_admin_assemblies.new_assembly_path
+
+      expect(page).to have_content("New assembly")
+    end
+  end
+end
