46a0b15b1c4c30286a4ee00f10ecf28eed184b24
Anna Topalidi <t.opalidisanna@gmail.com>
2025-11-17 12:21:10 +0100

    fix: restore editor image/video buttons

--- app/packs/entrypoints/decidim_editor.js
+++ app/packs/entrypoints/decidim_editor.js
@@ -1,4 +1,3 @@
-import createEditor from "src/decidim/decidim_awesome/editor";
 import DecidimKit from "src/decidim/editor/extensions/decidim_kit";
 
 // CSS
@@ -6,9 +5,3 @@ import "stylesheets/decidim/editor.scss"
 
 window.DecidimKit = DecidimKit;
 window.currentEditors = window.currentEditors || [];
-
-window.createEditor = (container) => {
-  let editor = createEditor(container);
-  window.currentEditors.push(editor);
-  return editor;
-}
