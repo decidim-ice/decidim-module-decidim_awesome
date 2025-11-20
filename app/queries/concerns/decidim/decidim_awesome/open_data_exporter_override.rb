d0c630a245a3341080fc1575b7fa4ad1786eaa6d
Anna Topalidi <t.opalidisanna@gmail.com>
2025-11-20 15:05:57 +0100

    fix OpenDataExporterOverride

--- app/queries/concerns/decidim/decidim_awesome/open_data_exporter_override.rb
+++ app/queries/concerns/decidim/decidim_awesome/open_data_exporter_override.rb
@@ -12,8 +12,6 @@ module Decidim
 
         alias_method :decidim_components, :components
         alias_method :decidim_participatory_spaces, :participatory_spaces
-        # to remove once https://github.com/decidim/decidim/pull/15459 is merged and backported
-        alias_method :decidim_data_for_participatory_space, :data_for_participatory_space
 
         def components
           @awesome_components ||= decidim_components.where.not(id: service.component_with_invisible_resources)
@@ -26,18 +24,6 @@ module Decidim
               invisible_space.exclude?(space)
             end
           end
-        end
-
-        # to remove once https://github.com/decidim/decidim/pull/15459 is merged and backported
-        def data_for_participatory_space(export_manifest)
-          collection = participatory_spaces.filter { |space| space.manifest.name == export_manifest.manifest.name }.flat_map do |participatory_space|
-            export_manifest.collection.call.where(id: participatory_space)
-          end
-          serializer = export_manifest.open_data_serializer.nil? ? export_manifest.serializer : export_manifest.open_data_serializer
-          exporter = Decidim::Exporters::CSV.new(collection, serializer)
-          get_help_definition(:spaces, exporter, export_manifest, collection.count) unless collection.empty?
-
-          exporter.export
         end
 
         def service
