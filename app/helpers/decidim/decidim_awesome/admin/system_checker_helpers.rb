41bc90e832bb01a8771a79d08d63a2f06c92fcaa
Laura J <75725233+laurajaime@users.noreply.github.com>
2026-02-19 16:58:08 +0100

    Remove migrate images checker (#512)

--- app/helpers/decidim/decidim_awesome/admin/system_checker_helpers.rb
+++ app/helpers/decidim/decidim_awesome/admin/system_checker_helpers.rb
@@ -23,17 +23,6 @@ module Decidim
         def valid?(spec, file)
           SystemChecker.valid?(spec, file)
         end
-
-        def images_migrated?
-          pending_image_migrations.zero?
-        end
-
-        def pending_image_migrations
-          @pending_image_migrations ||= begin
-            images = Decidim::DecidimAwesome::EditorImage.where(organization: current_organization)
-            images.count - images.joins(:file_attachment).count
-          end
-        end
       end
     end
   end
