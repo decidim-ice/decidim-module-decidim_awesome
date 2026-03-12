f05db21f3ee366012e9a140ac68d5a2042b1c01d
Ivan Vergés <ivan@pokecode.net>
2026-03-12 15:47:36 +0100

    go nuclear

--- app/cells/concerns/decidim/decidim_awesome/data_consent_cell_override.rb
+++ app/cells/concerns/decidim/decidim_awesome/data_consent_cell_override.rb
@@ -3,46 +3,24 @@
 module Decidim
   module DecidimAwesome
     module DataConsentCellOverride
-      def categories
-        return @categories if defined?(@categories)
+      # [ {
+      #    slug: "essential",
+      #    mandatory: true,
+      #    items: [
+      #      { type: "cookie", name: "_session_id", visible: false, essential: true, expiration: ... },
+      #      { type: "cookie", name: "decidim-consent" },
+      #      { type: "local_storage", name: "pwaInstallPromptSeen" }
+      #    ]
+      #  }, etc... ]
 
-        config_categories = categories_from_config
-        awesome = awesome_categories
-        @categories =
-          if config_categories.blank?
-            CookieManagementStore.new(model).stored_categories.map { |category| to_frontend_hash(category, model) }
-          else
-            awesome
-          end
+      def categories
+        @categories ||= CookieManagementStore.new(model, awesome_categories).categories
       end
 
       private
 
       def awesome_categories
-        all_categories = categories_from_config || []
-        visible_categories = all_categories.reject { |category| category["visibility"] == "hidden" }
-        visible_categories.map { |category| to_frontend_hash(category, model) }
-      end
-
-      def categories_from_config
-        store = CookieManagementStore.new(model)
-        store.stored_categories.presence
-      end
-
-      def to_frontend_hash(category_hash, organization)
-        slug = category_hash["slug"]
-        items = (category_hash["items"] || []).map do |item|
-          CookieItem.new(item, organization).to_frontend_hash
-        end
-        {
-          "slug" => slug,
-          "title" => translated_attribute(category_hash["title"], organization).presence ||
-            I18n.t("layouts.decidim.data_consent.modal.#{slug}.title"),
-          "description" => translated_attribute(category_hash["description"], organization).presence ||
-            I18n.t("layouts.decidim.data_consent.modal.#{slug}.description"),
-          "mandatory" => category_hash["mandatory"] || false,
-          "items" => items
-        }
+        Decidim::Decidim::DecidimAwesome::AwesomeConfig.find_by(organization: model, var: :cookie_management)&.value
       end
     end
   end
--- app/services/decidim/decidim_awesome/cookie_management_store.rb
+++ app/services/decidim/decidim_awesome/cookie_management_store.rb
@@ -5,53 +5,36 @@ module Decidim
     class CookieManagementStore
       include HasCookieCategories
 
-      def initialize(organization)
+      def initialize(organization, current_config = nil)
         @organization = organization
+        @current_config = current_config || {}
+        @decidim_defaults = Decidim.consent_categories || {}
       end
 
-      def current_categories
-        stored_categories.map { |hash| CookieCategory.new(hash, @organization) }
-      end
+      attr_reader :organization, :current_config, :decidim_defaults
 
-      def stored_categories
-        categories_data["categories"]
-      end
-
-      def find_category(slug)
-        category = stored_categories.find { |c| c["slug"].to_s == slug.to_s }
-        CookieCategory.new(category, @organization) if category
-      end
-
-      def save!(categories)
-        setting.value = { "categories" => categories }
-        setting.save!
-        @categories_data = nil
-      end
-
-      def ensure_initialized!
-        existing_slugs = stored_categories.map { |c| c["slug"].to_s }
-        missing_defaults = default_decidim_categories.reject { |c| existing_slugs.include?(c["slug"].to_s) }
-        return if missing_defaults.empty?
-
-        save!(stored_categories + missing_defaults)
+      def categories
+        @categories ||= @decidim_defaults.merge(current_config).map do |category|
+          sanitize_category(category)
+        end
       end
 
       private
 
-      def setting
-        @setting ||= AwesomeConfig.find_or_initialize_by(
-          var: :cookie_management,
-          organization: @organization
-        )
-      end
-
-      def categories_data
-        @categories_data ||= begin
-          data = setting.value
-          data = {} unless data.is_a?(Hash)
-          data["categories"] = [] unless data["categories"].is_a?(Array)
-          data
-        end
+      def sanitize_category(category)
+        {
+          slug: category["slug"],
+          mandatory: category["mandatory"] || false,
+          items: (category["items"] || []).map do |item|
+            {
+              type: item["type"],
+              name: item["name"],
+              visible: item.fetch("visible", true),
+              essential: item.fetch("essential", false),
+              expiration: item["expiration"]
+            }
+          end
+        }
       end
     end
   end
