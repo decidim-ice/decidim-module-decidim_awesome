664ef0916a7ac209cda2c39cb78b2f86e6fb4c66
David Igón <david@pokecode.net>
2026-04-21 12:14:15 +0200

    add commands to update and estroy auto moderation rule

---
+++ app/commands/decidim/decidim_awesome/admin/destroy_auto_moderation_rule.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Decidim
+    module DecidimAwesome
+        module Admin
+            class DestroyAutoModerationRule < Decidim::Command
+                include NeedsConstraintHelpers
+        
+                # Public: Initializes the command.
+                def initialize(rule_id, organization)
+                @rule_id = rule_id
+                @organization = organization
+                @config_var = :auto_moderation_rules
+                end
+        
+                # Executes the command. Broadcasts these events:
+                #
+                # - :ok when everything is valid.
+                # - :invalid if we couldn't proceed.
+                #
+                # Returns nothing   .
+                def call
+                    config = create_hash_config!()
+                    
+                    unless config.value.key?(rule_id)
+                        return broadcast(:invalid, "Rule not found")
+                    end
+
+                    config.value.delete(rule_id)
+                    config.save!
+                    
+                    broadcast(:ok)
+                end
+        
+                private
+        
+                attr_reader :rule_id
+            end
+        end
+    end
+end
---
+++ app/commands/decidim/decidim_awesome/admin/update_auto_moderation_rule.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Decidim
+    module DecidimAwesome
+        module Admin
+        class UpdateAutoModerationRule < Decidim::Command
+            include NeedsConstraintHelpers
+    
+            # Public: Initializes the command.
+            def initialize(form, rule_id, organization)
+            @form = form
+            @rule_id = rule_id
+            @organization = organization
+            @config_var = :auto_moderation_rules
+            end
+    
+            # Executes the command. Broadcasts these events:
+            #
+            # - :ok when everything is valid.
+            # - :invalid if we couldn't proceed.
+            #
+            # Returns nothing.
+            def call
+            return broadcast(:invalid) if form.invalid?
+    
+            config = create_hash_config!()
+            entry = form.to_params
+            rule_options = entry.delete("rule_options")
+
+            entry["rule_options"] = rule_options.split(",").map(&:strip).reject(&:empty?)    
+            
+            config.value[@rule_id] = entry
+            config.save!
+            broadcast(:ok, entry)
+            end
+    
+            private
+    
+            attr_reader :form, :rule_id
+        end
+        end
+    end
+    end
