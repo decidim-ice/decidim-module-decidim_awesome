ffdaf3189f36e7fc628a504bbccba75f48eaf385
David Igón <david@pokecode.net>
2026-04-28 18:14:58 +0200

    add spec to test word filter rule

--- spec/lib/moderation_actions/moderate_and_hide_spec.rb
+++ spec/lib/moderation_actions/moderate_and_hide_spec.rb
@@ -3,21 +3,21 @@
 require "spec_helper"
 
 module Decidim::DecidimAwesome
-    module ModerationActions
-        describe ModerateAndHide do
-            subject { described_class.new(resource) }
+  module ModerationActions
+    describe ModerateAndHide do
+      subject { described_class.new(resource) }
 
-            let!(:resource) { create(:comment) }
-            let(:organization) { resource.author.organization }
-            let!(:admin) { create(:user, :confirmed, :admin, organization:) }
+      let!(:resource) { create(:comment) }
+      let(:organization) { resource.author.organization }
+      let!(:admin) { create(:user, :confirmed, :admin, organization:) }
 
-            it "moderates the resource" do
-                expect do
-                    subject.execute
-                end.to change { resource.reports.count }.from(0).to(1)
+      it "moderates the resource" do
+        expect do
+          subject.execute
+        end.to change { resource.reports.count }.from(0).to(1)
 
-                expect(resource.moderation.hidden_at).not_to be_nil
-            end
-        end
+        expect(resource.moderation.hidden_at).not_to be_nil
+      end
     end
+  end
 end
---
+++ spec/lib/moderation_rules/word_filter_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::DecidimAwesome
+  module ModerationRules
+    describe WordFilter do
+      subject { described_class.new(options) }
+
+      let!(:comment) { create(:comment, body:) }
+      let(:body) { { "en" => "badword1" } }
+      let(:options) { ["badword1"] }
+
+      context "when the resource has a banned word" do
+        it "filters the body based on the options" do
+          expect(subject.check(comment)).to be_truthy
+        end
+      end
+
+      context "when the resource does not have a banned word" do
+        let(:options) { ["badword2"] }
+
+        it "does not filter the body content" do
+          expect(subject.check(comment)).to be_falsey
+        end
+      end
+    end
+  end
+end
