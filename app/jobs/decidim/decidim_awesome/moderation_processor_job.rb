9671be22303489c5e1e0a9980cab9cadb9492a57
David Igón <david@pokecode.net>
2026-04-28 12:35:28 +0200

    fix moderation processor

--- app/jobs/decidim/decidim_awesome/moderation_processor_job.rb
+++ app/jobs/decidim/decidim_awesome/moderation_processor_job.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Decidim
-    module DecidimAwesome
-        class ModerationProcessorJob < Decidim::ApplicationJob
-            queue_as :default
+  module DecidimAwesome
+    class ModerationProcessorJob < Decidim::ApplicationJob
+      queue_as :default
 
-            def perform(proposal)
-                Decidim::DecidimAwesome::Processors::ModerationProcessor.new(proposal).process
-            end
-        end
+      def perform(object)
+        Decidim::DecidimAwesome::Processors::ModerationProcessor.process(object)
+      end
     end
+  end
 end
--- lib/decidim/decidim_awesome/processors/moderation_processor.rb
+++ lib/decidim/decidim_awesome/processors/moderation_processor.rb
@@ -1,77 +1,83 @@
 # frozen_string_literal: true
 
 module Decidim
-    module DecidimAwesome
-        module Processors
-            class ModerationProcessor
-                def initialize(object)
-                    @object = object
-                end
+  module DecidimAwesome
+    module Processors
+      class ModerationProcessor
+        include AwesomeHelpers
 
-                def self.process(object)
-                    byebug
-                    processor = new object
-                    return unless processor.valid_object?
-
-                    config = processor.awesome_config
-                    return unless config&.value
-
-                    rules = config.value.select { |_id, rule| rule["enabled"] }
-                    return if rules.empty?
-
-                    rules.each do |rule_id, rule|
-                        rule_manifest = get_rule_manifest(rule["rule_type"])
-                        checker_class = rule_manifest.checker_class.constantize
-                        checker = checker_class.new(rule["rule_options"])
-                        next unless checker.check(@object)
-
-                        targets = rule["targets"]
-                        targets.each do |_id, target|
-                            action_manifest = get_action_manifest(target["action_type"])
-                            handler_class = action_manifest.handler_class.constantize
-                            handler = handler_class.new(target["action_options"])
-                            handler.execute(@object)
-                        end
-                    end
-                end
-
-                def get_action_manifest(action_type)
-                    Decidim::DecidimAwesome.moderation_actions_registry.manifests.find { |manifest| manifest.name.to_s == action_type }
-                end
-
-                def get_rule_manifest(rule_type)
-                    Decidim::DecidimAwesome.moderation_rules_registry.manifests.find { |manifest| manifest.name.to_s == rule_type }
-                end
-
-                def valid_object?
-                    case @object
-                    when Decidim::Proposals::Proposal
-                        :proposals
-                    when Decidim::Comments::Comment
-                        :comments
-                    end
-                end
-
-                def rules_applies_to_object?(rule)
-                    rule["targets"].any? do |_id, target|
-                        target["object_type"] == object_type && target["action_type"] == action_type
-                    end
-                end
-
-                def object_type
-                    case @object
-                    when Decidim::Proposals::Proposal
-                        "proposal"
-                    when Decidim::Comments::Comment
-                        "comment"
-                    end
-                end
-
-                def awesome_config
-                    organization = @object.organization
-                    @config ||= Decidim::DecidimAwesome::AwesomeConfig.find_by(var: :auto_moderation_rules, organization:)
-                end
-            end
+        def initialize(object)
+          @object = object
         end
+
+        def self.process(object)
+          processor = new object
+          return unless processor.valid_object?
+
+          config = processor.config
+          return unless config
+
+          config.context_from_component!(object.component)
+
+          return unless config.constrained_in_context?(:auto_moderation_rules)
+
+          rules = config.setting_for(:auto_moderation_rules).value.select { |_id, rule| rule["enabled"] }
+          return if rules.empty?
+
+          # validate agains constraints
+          rules.each do |_rule_id, rule|
+            rule_manifest = processor.get_rule_manifest(rule["rule_type"])
+            checker_class = rule_manifest.checker_class.constantize
+            checker = checker_class.new(rule["rule_options"])
+            next unless checker.check(object)
+
+            targets = rule["targets"]
+            targets.each do |_id, target|
+              action_manifest = processor.get_action_manifest(target["action_type"])
+              handler_class = action_manifest.handler_class.constantize
+              handler = handler_class.new(object, target["action_options"])
+              handler.execute
+            end
+          end
+        end
+
+        def get_action_manifest(action_type)
+          Decidim::DecidimAwesome.moderation_actions_registry.manifests.find { |manifest| manifest.name.to_s == action_type }
+        end
+
+        def get_rule_manifest(rule_type)
+          Decidim::DecidimAwesome.moderation_rules_registry.manifests.find { |manifest| manifest.name.to_s == rule_type }
+        end
+
+        def valid_object?
+          case @object
+          when Decidim::Proposals::Proposal
+            :proposals
+          when Decidim::Comments::Comment
+            :comments
+          end
+        end
+
+        def rules_applies_to_object?(rule)
+          rule["targets"].any? do |_id, target|
+            target["object_type"] == object_type && target["action_type"] == action_type
+          end
+        end
+
+        def object_type
+          case @object
+          when Decidim::Proposals::Proposal
+            "proposal"
+          when Decidim::Comments::Comment
+            "comment"
+          end
+        end
+
+        def config
+          organization = @object.organization
+          @config ||= Decidim::DecidimAwesome::Config.new(organization)
+        end
+      end
     end
+  end
 end
