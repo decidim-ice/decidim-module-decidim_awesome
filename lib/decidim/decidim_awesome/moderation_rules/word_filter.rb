5c2820fe8df707b38d7310ae8a5429f4fc451bd5
David Igón <david@pokecode.net>
2026-05-04 18:03:16 +0200

    add more specific tests for word_filter

--- lib/decidim/decidim_awesome/moderation_rules/word_filter.rb
+++ lib/decidim/decidim_awesome/moderation_rules/word_filter.rb
@@ -14,10 +14,8 @@ module Decidim
           return false unless object.respond_to?(:body)
 
           @options.any? do |word|
-            return true if object.body.values.any? { |v| v.include?(word) }
+            object.body.values.any? { |v| v.include?(word) }
           end
-
-          false
         end
       end
     end
--- spec/lib/moderation_rules/word_filter_spec.rb
+++ spec/lib/moderation_rules/word_filter_spec.rb
@@ -17,11 +17,27 @@ module Decidim::DecidimAwesome
         end
       end
 
+      context "when the wording is phrased" do
+        let(:body) { { "en" => "is this a badword1?" } }
+        it "matches the wording" do
+          expect(subject.check(comment)).to be_truthy
+        end
+      end
+
       context "when the resource does not have a banned word" do
         let(:options) { ["badword2"] }
 
         it "does not filter the body content" do
           expect(subject.check(comment)).to be_falsey
+        end
+      end
+
+      context "when there are multiple words to filter" do
+        let(:options) { ["badword1", "badword2"]}
+        let(:body) { { "en" => "It has badword1 and not badword_2" } }
+
+        it "detecs the word inside" do
+          expect(subject.check(comment)).to be_truthy
         end
       end
     end
