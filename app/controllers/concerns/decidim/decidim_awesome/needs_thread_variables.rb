e092ebda834e09bc9620d6e0151df224b99ccc90
Ivan Vergés <ivan@pokecode.net>
2026-07-03 11:07:28 +0200

    rename methods

--- app/controllers/concerns/decidim/decidim_awesome/needs_thread_variables.rb
+++ app/controllers/concerns/decidim/decidim_awesome/needs_thread_variables.rb
@@ -6,19 +6,19 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        around_action :with_thread_organization
+        around_action :with_thread_variables
       end
 
       private
 
-      def with_thread_organization
-        set_thread_organization
+      def with_thread_variables
+        set_thread_variables
         yield
       ensure
-        clear_thread_organization
+        clear_thread_variables
       end
 
-      def set_thread_organization
+      def set_thread_variables
         return unless respond_to?(:current_organization)
 
         config = Decidim::DecidimAwesome::AwesomeConfig.find_by(
@@ -28,7 +28,7 @@ module Decidim
         Thread.current[:awesome_authorization_handler] = config&.value
       end
 
-      def clear_thread_organization
+      def clear_thread_variables
         Thread.current[:awesome_authorization_handler] = nil
       end
     end
