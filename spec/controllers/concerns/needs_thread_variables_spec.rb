80e2c0458a3fd1cd19ee0777e06501ef4ecd924c
Ivan Vergés <ivan@pokecode.net>
2026-07-03 11:28:18 +0200

    fix spec

--- spec/controllers/concerns/needs_thread_variables_spec.rb
+++ spec/controllers/concerns/needs_thread_variables_spec.rb
@@ -22,17 +22,17 @@ module Decidim
       let(:controller) { controller_class.new }
       let(:organization) { create(:organization) }
 
-      describe "#set_thread_organization" do
+      describe "#set_thread_variables" do
         it "sets Thread.current[:awesome_authorization_handler] to nil when no config exists" do
           controller.organization = organization
-          controller.send(:set_thread_organization)
+          controller.send(:set_thread_variables)
 
           expect(Thread.current[:awesome_authorization_handler]).to be_nil
         end
 
         it "does not set when current_organization is not available" do
           controller.organization = nil
-          controller.send(:set_thread_organization)
+          controller.send(:set_thread_variables)
 
           expect(Thread.current[:awesome_authorization_handler]).to be_nil
         end
@@ -44,20 +44,20 @@ module Decidim
 
           it "sets the config value" do
             controller.organization = organization
-            controller.send(:set_thread_organization)
+            controller.send(:set_thread_variables)
 
             expect(Thread.current[:awesome_authorization_handler]).to eq({ "name" => "Custom Name", "explanation" => "Custom Explanation" })
           end
         end
       end
 
-      describe "#clear_thread_organization" do
+      describe "#clear_thread_variables" do
         before do
           Thread.current[:awesome_authorization_handler] = { name: "Test" }
         end
 
         it "clears Thread.current[:awesome_authorization_handler]" do
-          controller.send(:clear_thread_organization)
+          controller.send(:clear_thread_variables)
 
           expect(Thread.current[:awesome_authorization_handler]).to be_nil
         end
@@ -65,14 +65,14 @@ module Decidim
 
       context "when included in a controller" do
         it "adds around_action callback" do
-          expect(controller_class._process_action_callbacks.map(&:filter)).to include(:with_thread_organization)
+          expect(controller_class._process_action_callbacks.map(&:filter)).to include(:with_thread_variables)
         end
 
         it "clears thread variable even when action raises" do
           controller.organization = organization
           Thread.current[:awesome_authorization_handler] = { name: "Test" }
 
-          expect { controller.send(:with_thread_organization) { raise StandardError, "test error" } }.to raise_error(StandardError)
+          expect { controller.send(:with_thread_variables) { raise StandardError, "test error" } }.to raise_error(StandardError)
 
           expect(Thread.current[:awesome_authorization_handler]).to be_nil
         end
