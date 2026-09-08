7f388971d66ccb6b8d67d3b8136072bcbbae756e
David Igón <david@pokecode.net>
2026-09-08 16:14:50 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check all the gems are installed or fails.
-
-if [ bundle --check ]; then
-  echo "✅ All gems are installed"
+bundle check
+if [ $? -ne 0 ]; then
+  echo "❌ Gems in Gemfile are not installed, aborting..."
+  bundle install --jobs 4 --retry 3
 else
-  echo "❌ Some gems are missing, installing..."
-  bundle install
+  echo "✅ Gems in Gemfile are installed"
 fi
 
 decidim /module_app --path  /app --skip_spring --skip_gemfile --demo --locales="en,ca,es" --queue=sidekiq --recreate_db --seed_db --force-ssl false
